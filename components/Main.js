f7645cde425eca8929c4166a25ff9691fb810220
Nathan Willson <willson.b.nathan@gmail.com>
2025-03-24 10:59:08 +0900

    bye

--- components/Main.js
+++ components/Main.js
@@ -5,6 +5,15 @@ import ScheduleTable from './common/ScheduleTable'
 export default function Main() {
   return (
     <main>
+      <div style={{
+        padding: 10,
+        margin: 10,
+        color: 'white',
+        backgroundColor: '#FF6961',
+        textAlign: 'center',
+      }}>
+        Sorry, this site costs too much to run b/c of GoogleMaps API limits. I'm shutting it down for now.
+      </div>
       <div>
         <ZonesPanel />
         <ToggleTimeFormat />
